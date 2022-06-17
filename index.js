f3ed60593759cc35853d38026ce5514b8c64ca00
Julliane Freitas <jullianefreitas@gmail.com>
2022-06-16 23:57:01 -0300

    fix error export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export function createUUID() {
+function createUUID() {
         let dt = new Date().getTime();
         const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
           const r = (dt + Math.random() * 16) % 16 | 0;
